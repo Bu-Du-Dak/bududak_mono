883ff78d8aeca3b310f39bcfb046f0612b4412b5
Bu-Du-Dak <guri930219@gmail.com>
2026-03-20 17:50:38 +0900

    3월 3주차 블로그

---
+++ apps/portfolio/src/constants/projects.ts
@@ -0,0 +1,62 @@
+import { REABLE_HOMEPAGE_STACKS, SAVEE_STACKS } from "./stacks";
+
+export const PROJECTS = [
+  {
+    title: "2022",
+    description: "주식회사 딩코 재직",
+    items: [
+      {
+        id: "test",
+        title: "내가만든 프로젝트 01",
+        description:
+          "이거는 이런이런 생각을 가지고 이러면 좋겠다 해서 이렇게 만들어보자 해서 이렇게 만들게된 프로젝트입니다.",
+        imgUrl: "/test01.jpg",
+        tags: REABLE_HOMEPAGE_STACKS,
+      },
+      {
+        id: "test2",
+        title: "내가만든 프로젝트 02",
+        description:
+          "이거는 이런이런 생각을 가지고 이러면 좋겠다 해서 이렇게 만들어보자 해서 이렇게 만들게된 프로젝트입니다.",
+        imgUrl: "/test02.jpg",
+        tags: SAVEE_STACKS,
+      },
+      {
+        id: "test3",
+        title: "qwe",
+        description: "agsags",
+        imgUrl: "/test02.jpg",
+        tags: ["Next.js", "TypeScript"],
+      },
+    ],
+  },
+  {
+    title: "2021",
+    description: "",
+    items: [
+      {
+        id: "test",
+        title: "내가만든 프로젝트 01",
+        description:
+          "이거는 이런이런 생각을 가지고 이러면 좋겠다 해서 이렇게 만들어보자 해서 이렇게 만들게된 프로젝트입니다.",
+        imgUrl: "/test01.jpg",
+        tags: REABLE_HOMEPAGE_STACKS,
+      },
+      {
+        id: "test2",
+        title: "내가만든 프로젝트 02",
+        description:
+          "이거는 이런이런 생각을 가지고 이러면 좋겠다 해서 이렇게 만들어보자 해서 이렇게 만들게된 프로젝트입니다.",
+        imgUrl: "/test02.jpg",
+        tags: SAVEE_STACKS,
+      },
+      {
+        id: "test3",
+        title: "qwe",
+        description: "agsags",
+        imgUrl: "/test02.jpg",
+        tags: ["Next.js", "TypeScript"],
+      },
+    ],
+  },
+];
